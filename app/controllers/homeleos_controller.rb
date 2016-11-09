2927567a260fa46d4256de7db7bc068c2763e58d
rushiliwei <oneinallinone@qq.com>
2016-11-09 10:14:51 +0800

    fixed

--- app/controllers/homeleos_controller.rb
+++ app/controllers/homeleos_controller.rb
@@ -1,6 +1,6 @@
 class HomeleosController < ApplicationController
-  before_filter :authenticate_user!  #添加这行
-  # skip_before_filter :authenticate_user! # tiaoguo yonghu denglu yanzheng
+  # before_filter :authenticate_user!  #添加这行
+  skip_before_filter :authenticate_user! # tiaoguo yonghu denglu yanzheng
   # load_and_authorize_resource :class => Qxrz::Homeleo
   load_and_authorize_resource :class => Competence::Homeleo
 
